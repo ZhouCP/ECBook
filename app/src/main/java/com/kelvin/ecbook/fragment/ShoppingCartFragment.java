464795483a99690907912ab5f123358b3b295972
Coding-LPD <edwardlink@foxmail.com>
2016-05-01 22:30:59 +0800

    Revert "complete ebook upload and home page"
    This reverts commit e0de2499ac3a2e189f37a7ef3b84c5fccd4e9c3c.

--- app/src/main/java/com/kelvin/ecbook/fragment/ShoppingCartFragment.java
+++ app/src/main/java/com/kelvin/ecbook/fragment/ShoppingCartFragment.java
@@ -6,8 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.kelvin.ecbook.R;
 
@@ -15,9 +13,6 @@ import com.kelvin.ecbook.R;
  * Created by kelvin on 16/4/19.
  */
 public class ShoppingCartFragment extends Fragment {
-
-    private TextView title;
-    private ImageView back;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,12 +24,6 @@ public class ShoppingCartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View mainView = inflater.inflate(R.layout.fragment_shoppingcart,null);
-
-        title = (TextView) mainView.findViewById(R.id.top_view_text);
-        title.setText("我的收藏");
-
-        back = (ImageView) mainView.findViewById(R.id.top_view_back);
-        back.setVisibility(View.GONE);
 
         return mainView;
     }
